0a8eaf729e43cdc1cfdc09290f738a0cf56d6532
Brayan Lopez <brayan.metallicalml@gmail.com>
2026-05-22 15:36:10 -0500

    feact: creacion de diseños en CSS y parte inicial del JS

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ console.log("Input", inputBusqueda);
 console.log("Botón", btnBuscar);
 console.log("Div:", divResultados);
 
-// Event listener para el botón
-botonBuscar.addEventListener("click", function () {
-    console.log("¡Hiciste click!");
-    console.log("Texto escrito:", inputBusqueda.value);
-});
+const shows = [];
+
+const query = inputBusqueda.value.trim()
+const API_KEY = 'e1ecc8e2bd5ae8552c91d9d04fe6bee8';
+const API_URL = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${e1ecc8e2bd5ae8552c91d9d04fe6bee8}`;
+
 
